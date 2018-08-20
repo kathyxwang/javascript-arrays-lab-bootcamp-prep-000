60cce25aac76d3476f1ce9bd1a2fca7145e1fbdd
Kathy Wang <kathy.x.wang@gmail.com>
2018-08-20 20:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  name.push("kitten");
+  name.push();
+  return name;
+}
+
+function destructivelyPrependKitten(name) {
+  name.shift();
+  return name;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  name.pop();
+  return name;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  name.unshift();
   return name;
 }
